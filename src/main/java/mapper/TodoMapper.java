ee5cd2e8214442895ca424c693062ce2fe7138e6
koya0713 <142728767+koya0713@users.noreply.github.com>
2024-02-16 08:12:06 +0900

    todoアプリ

---
+++ src/main/java/mapper/TodoMapper.java
@@ -0,0 +1,24 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import entity.Todo;
+
+@Mapper
+public interface TodoMapper {
+
+    public List<Todo> selectAll();
+
+    public List<Todo> selectIncomplete();
+
+    public List<Todo> selectComplete();
+
+    public void add(Todo todo);
+
+    public void update(Todo todo);
+
+    public void delete();
+}
+
